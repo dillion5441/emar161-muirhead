7fde33da024d8ab41325e788d98ff9c29fd66841
dillion5441 <83729599+dillion5441@users.noreply.github.com>
2021-05-05 15:12:29 -0500

    Initial upload of my sketch

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 let img;
 
 function preload() {
-img = loadImage('House Frame 1-min.png');
+img = loadImage('House Frame 2-min.png');
 }
 
 function setup() {
@@ -15,132 +15,148 @@ image(img, 0, 0);
 
 ////structure
 
-line(40, 150, 200, 145);
+line(120, 164, 120, 220);
 strokeWeight(4);
 stroke('yellow');
 
-line(55, 150, 55, 220 );
+line(103, 163, 132, 160);
 strokeWeight(4);
 stroke('yellow');
 
-line(200, 145, 305, 148);
+line(132, 160, 160, 160);
 strokeWeight(4);
 stroke('yellow');
 
- line(305, 148, 305, 120);
+ line(160, 160, 200, 156);
 strokeWeight(4);
 stroke('yellow');
 
- line(305, 120, 453, 127);
+  line(200, 156, 295, 156);
 strokeWeight(4);
 stroke('yellow');
 
-  line(453, 127, 470, 122);
+ line(295, 156, 295, 130);
 strokeWeight(4);
 stroke('yellow');
 
-line(470, 122, 550, 126);
+ line(285, 130, 435, 130);
 strokeWeight(4);
 stroke('yellow');
 
-line(550, 126, 548, 154);
+  line(435, 130, 443, 125);
 strokeWeight(4);
 stroke('yellow');
 
-line(548, 154, 610, 158);
+
+  line(442, 125, 540, 125);
 strokeWeight(4);
 stroke('yellow');
 
-  line(610, 158, 675, 155);
+
+  line(532, 125, 529, 153);
 strokeWeight(4);
 stroke('yellow');
 
- line(670, 155, 670, 188 );
+line(529, 153, 630, 153);
 strokeWeight(4);
 stroke('yellow');
 
- line(670, 188, 692, 188 );
+line(630, 153, 675, 150);
 strokeWeight(4);
 stroke('yellow');
 
- line(692, 188, 692, 270);
+  line(675, 150, 722, 160);
 strokeWeight(4);
 stroke('yellow');
+
+  line(710, 160, 710, 215);
+strokeWeight(4);
+stroke('yellow');
+
+
 
 ////posts
 
-  line(220, 223, 220, 280);
+line(203, 223, 203, 270);
 strokeWeight(4);
 stroke('yellow');
 
- line(315, 220, 315, 280);
+line(275, 223, 275, 274);
 strokeWeight(4);
 stroke('yellow');
 
-   line(334, 222, 334, 280);
+ line(290, 223, 290, 274);
 strokeWeight(4);
 stroke('yellow');
 
- line(408, 222, 408, 280);
+line(363, 223, 363, 274);
 strokeWeight(4);
 stroke('yellow');
 
-  line(424, 222, 424, 280);
+line(379, 223, 379, 274);
 strokeWeight(4);
 stroke('yellow');
 
-line(494, 222, 494, 275);
+line(450, 223, 450, 274);
 strokeWeight(4);
 stroke('yellow');
 
- line(510, 222, 510, 275);
+line(464, 223, 464, 274);
 strokeWeight(4);
 stroke('yellow');
 
 
- line(570, 222, 570, 273);
+line(536, 223, 536, 274);
 strokeWeight(4);
 stroke('yellow');
 
- line(585, 222, 585, 273);
+line(549, 223, 549, 274);
 strokeWeight(4);
 stroke('yellow');
 
-  line(622, 222, 622, 270);
+line(603, 223, 603, 274);
 strokeWeight(4);
 stroke('yellow');
 
- line(635, 222, 635, 270);
+line(619, 223, 619, 274);
 strokeWeight(4);
 stroke('yellow');
 
-line(680, 222, 680, 270);
+line(678, 223, 678, 274);
+strokeWeight(4);
+stroke('yellow');
+
+line(693, 222, 693, 270);
+strokeWeight(4);
+stroke('yellow');
+
+line(693, 222, 710, 216);
 strokeWeight(4);
 stroke('yellow');
 
 ///post tops
 
-line(220, 223, 313, 222);
+line(203, 223, 275, 222);
 strokeWeight(4);
 stroke('yellow');
 
-line(334, 222, 405, 222);
+line(290, 222, 362, 222);
 strokeWeight(4);
 stroke('yellow');
 
- line(425, 222, 492, 222);
+line(380, 222, 449, 222);
 strokeWeight(4);
 stroke('yellow');
 
-  line(512, 222, 570, 222);
+line(465, 222, 535, 222);
 strokeWeight(4);
 stroke('yellow');
 
-  line(587, 222, 620, 222);
+line(550, 222, 603, 222);
 strokeWeight(4);
 stroke('yellow');
 
- line(637, 222, 678, 222);
+line(620, 222, 677, 222);
 strokeWeight(4);
 stroke('yellow');
 
@@ -149,6 +165,6 @@ stroke('yellow');
 fill(0);
 textSize(32);
 textFont('bebas kai');
-text('House Frame 1', 620, 430);
+text('House Frame 2', 620, 430);
 
 }
